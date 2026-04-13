26b88a6d54708070819ade42d528918f504b183b
Ryan <ariyoquadri4@gmail.com>
2026-04-13 14:56:46 +0100

    Nothing

--- PythonTasks/Assignments/Chapter 2 na bread/TimeConverter.py
+++
@@ -1,7 +0,0 @@
-seconds = int(input("Enter the number of seconds"))
-
-hours = seconds // 3600
-minutes = (seconds % 3600) // 60
-seconds = seconds % 60
-
-print("The number is ", hours, "Hours ", minutes, "Minutes and", seconds, "Seconds")
---
+++ PythonTasks/Classworks/Swap.py
@@ -0,0 +1,17 @@
+"""a = 5
+b = 10
+
+if (a == 5):
+    a = 10
+
+if (b == 10):
+    b = 5"""
+
+
+a = 5
+b = 10
+a,b = b,a
+print('a =', a, 'b =', b)
+
+#i swapped it using the if statement because after we assigned the variables a and b they are given a single number so i used that number to make an if statemennt and swap the values of both variables if the condiions are true which they are because redeclaring the variables with themselves just prints out one number
+
